c67852be9938483c3b6f1ceae2a487612459bb12
Tariku Lemma <tlemma@excellerentsolutions.com>
2022-12-02 12:53:16 +0300

    initial commit

---
+++ src/main/java/com/tarikukebede/sunscription/SunscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.tarikukebede.sunscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunscriptionApplication.class, args);
+	}
+
+}
